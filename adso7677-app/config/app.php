88f7c609d71e47be0b78ca3e5318a39826271f9f
arbonaisrael <israelarbona1988@gmail.com>
2024-11-26 20:46:29 -0500

    sesion sincroninca manejo de grilla con javascript y libreria dtatables js 26/11/2024

--- adso7677-app/config/app.php
+++ adso7677-app/config/app.php
@@ -197,6 +197,8 @@ return [
 
         Laraveles\Spanish\SpanishServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
+    
 
     ],
 
